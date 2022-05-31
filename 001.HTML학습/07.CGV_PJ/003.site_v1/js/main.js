94872dce8216c9a91ade15ddcd07574f63bf1e35
sn1091 <104745494+sn1091@users.noreply.github.com>
2022-05-31 09:33:51 +0900

    0531

--- 001.HTML학습/07.CGV_PJ/003.site_v1/js/main.js
+++ 001.HTML학습/07.CGV_PJ/003.site_v1/js/main.js
@@ -1,20 +1,25 @@
-// CGV 메인 페이지 JS - main.js
+//cgv 메인 페이지 JS - main.js
 
-/*************************************** 
-    함수명: chgMV
-    기능: 영화예고편을 변경함
-***************************************/
-function chgMV(mvid){ // mvid - 영화아이디값 전달변수
+/**************************************** 
+    함수명 : chgMV
+    기능 : 영화 예고편 선택하여 플레이하기
 
-    // 1. 함수호출 확인!
-    console.log("나야나!", mvid);
+****************************************/
 
-    // 2. 대상선정: #screen iframe
+function chgMV(vid){ //vid - 동영상아이디
+    // 1. 함수 호출 확인
+    console.log("바꿔!",vid);
+
+    // 2. 변경 대상 : #screen iframe
     var tg = document.querySelector("#screen iframe");
 
-    // 3. 변경내용: 대상의 src속성 변경하기
-    // src중 영화 아이디부분을 변경함!
-    tg.src = `https://www.youtube.com/embed/${mvid}?autoplay=1&playsinline=1`;
+    // 3. 변경내용 : src속성 재설정하기
+    // 선택요소.src = 변경할 값
 
+    tg.src = `https://www.youtube.com/embed/
+    ${vid}?autoplay=1&mute=1&loop=1
+    &playlist=${vid}&playsinline=1`;
 
-} ////////// chgMV 함수 ////////////////////
+}
+////////////////// chgMV 함수 /////////////
+///////////////////////////////////////////
