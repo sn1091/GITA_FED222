edf8737ba3cb5514a78a98f7526b3dbc29042b79
sn1091 <104745494+sn1091@users.noreply.github.com>
2022-06-15 14:00:16 +0900

    0615

--- 001.HTML학습/07.CGV_PJ/003.site_v2/js/main.js
+++ 001.HTML학습/07.CGV_PJ/003.site_v2/js/main.js
@@ -1,5 +1,28 @@
 //cgv 메인 페이지 JS - main.js
 
+window.addEventListener("DOMContentLoaded",()=>{
+    console.log("로드완료")
+
+// 영화 선택 리스트: sml(select Movie List)
+let sml = document.querySelectorAll(".mlist ul>li");
+console.log("영화선택리스트:",sml);
+
+// 선택요소들의 노드리스트 컬렉션을 모두 click설정함!
+for(let x of sml){ // x - 각 li 요소
+    x.onclick = ( ) =>{
+    // 모든 요소 클래스 "on" 지우기
+    for(let z of sml) 
+    z.classList.remove("on");
+
+    // 해당요소에 클래스 넣기
+    // classList의 메서드 add()
+    x.classList.add("on");
+
+    } ///// click /////
+
+} ///// for of //////////
+
+}); //////// load /////////
 /**************************************** 
     함수명 : chgMV
     기능 : 영화 예고편 선택하여 플레이하기
